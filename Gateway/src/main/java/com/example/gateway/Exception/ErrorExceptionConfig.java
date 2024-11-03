90f4803ca152d2584db7733d53df3488d7a355a9
Kim Gwang Je <gwangjeg14@gmail.com>
2024-11-03 20:46:09 +0900

    Feat: "Gateway Exception Handler develop"

---
+++ Gateway/src/main/java/com/example/gateway/Exception/ErrorExceptionConfig.java
@@ -0,0 +1,17 @@
+package com.example.gateway.Exception;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class ErrorExceptionConfig {
+    private final ObjectMapper objectMapper;
+    @Bean
+    public ErrorWebExceptionHandler globalExceptionHandler() {
+        return new GlobalExceptionHandler(objectMapper);
+    }
+}
